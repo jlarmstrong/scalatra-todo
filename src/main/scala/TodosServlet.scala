858857e159f7accee91e23ef6f1848e84b2113c8
Jared Armstrong <jrod@jaredarmstrong.name>
2011-12-16 11:05:31 -0500

    example log akka

--- src/main/scala/TodosServlet.scala
+++ src/main/scala/TodosServlet.scala
@@ -5,15 +5,32 @@ import todo.models._
 import _root_.akka.dispatch._
 import org.scalatra.akka.AkkaSupport
 
+import org.slf4j.{LoggerFactory}
+
 trait TodosServlet extends AppTrait {
   
   get("/todo/?") {
     Future {
-    println("inside todo")
+    logger.info("inside get(todo)")
 
     contentType = "text/html"
 
     layoutTemplate("/WEB-INF/views/todo.ssp",("layout" -> "/WEB-INF/layouts/default.ssp"))
+
+    }
+  }
+  
+  get("/todo/list/?"){
+    Future {
+      logger.info("inside get(todo/list)")
+      // return json
+    }
+  }
+  
+  put("/todo/?"){
+    Future {
+
+      logger.info("inside put(todo)")
 
     }
   }
